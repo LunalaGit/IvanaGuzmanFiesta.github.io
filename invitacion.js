bc4780b4492868ae4399ecc9c70d48cb26c0da74
nico9812 <nicocalderon9818@gmail.com>
2024-10-08 20:33:43 -0300

    [add]

--- invitacion.js
+++ invitacion.js
@@ -1,32 +1,48 @@
-
 document.addEventListener("DOMContentLoaded", function() {
     const form = document.getElementById("form");
     form.addEventListener("submit",function(event){
         event.preventDefault();
         const formData = new URLSearchParams();
-        formData.append("entry.851951850",document.getElementById("nombre").value);
-        // formData.append("entry.512626343","si");
-        formData.append("entry.512626343","no");
-        fetch("https://docs.google.com/forms/d/e/1FAIpQLSekp6A836wLEbD7Jj6WoN_Ntabd64vqzL8X10DCnh11D9d83g/formResponse",{
-            method: "POST",
-            body:formData,
-            headers:{
-                "Content-Type":"application/x-www-form-urlencoded"
-            },
-            mode:"no-cors"
-        }).then(()=>{
-            form.reset()
-            cerrarmodal();
-            abrirmodalok();
-            setTimeout(cerrarmodalok, 2000);
+        var nombre = document.getElementById("nombre").value;
+        if(nombre || nombre!=""){
+            formData.append("entry.851951850",nombre);
+            // formData.append("entry.512626343","si");
+            formData.append("entry.512626343","no");
+            fetch("https://docs.google.com/forms/d/e/1FAIpQLSekp6A836wLEbD7Jj6WoN_Ntabd64vqzL8X10DCnh11D9d83g/formResponse",{
+                method: "POST",
+                body:formData,
+                headers:{
+                    "Content-Type":"application/x-www-form-urlencoded"
+                },
+                mode:"no-cors"
+            }).then(()=>{
+                form.reset()
+                cerrarmodal();
+                abrirmodalok();
+                setTimeout(cerrarmodalok, 2000);
+    
+            }).catch(error =>{
+                const span = document.getElementById("spanerror");
+                span.style.display="block";
+            });
+        }
+        else{
+            abrirmodalerror();
+            setTimeout(cerrarmodalerror, 1000);
 
-        }).catch(error =>{
-            const span = document.getElementById("spanerror");
-            span.style.display="block";
-        });
+        }
     });
 
 });
+function abrirmodalerror(){
+    const modal = document.getElementById("modalerror");
+    modal.style.display="flex";
+}
+function cerrarmodalerror(){
+    const modal = document.getElementById("modalerror");
+    modal.style.display="none";
+}
+
 function abrirmodalok(){
     const modal = document.getElementById("modalok");
     modal.style.display="flex";
